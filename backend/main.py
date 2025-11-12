2a48bfb03c5cbd2ea09734d136f81e0c4543c2ec
Daniel Tzul <dtzul8@gmail.com>
2025-11-11 19:18:38 -0600

    fixed cors section in main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ class ChatRequest(BaseModel):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"),
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
